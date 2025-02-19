cc6ea33f69151a345cf3665426d35f2569b4b252
Bimla-jain-555 <vj13798@gmail.com>
2025-02-19 20:31:22 +0530

    add counter

--- src/components/KickBox.tsx
+++ src/components/KickBox.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React , {useState} from 'react';
 import CustomButton from './common/CommonButton';
-import {STATS} from '../utils/helper'
+import { STATS } from '../utils/helper'
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 
 const KickBox = () => {
+    const [count, setCount] = useState(false);
+    const { ref } = useInView({
+        triggerOnce: false,
+        onChange: (inView) => setCount(inView),
+    });
     return (
         <div id='stories' className='lg:pt-[134px] lg:pb-[128px] md:py-20 py-12'>
             <div className="container max-w-[1140px] mx-auto px-4">
@@ -12,11 +19,35 @@ const KickBox = () => {
                     Since 2019, our Kickbox Intrapreneurship Program has been inspiring employees across all divisions to turn their ideas into impactful solutions. From validating ideas (RedBox) to piloting projects (BlueBox) and driving implementation (GoldBox), Kickbox provides the tools and support to shape the future of Implenia. Become an intrapreneur yourself and bring your idea to life!
                 </p>
                 <div className="flex flex-wrap xl:justify-between justify-center md:gap-[77px] gap-[50px] pt-10 items-center">
-                    <div className='flex flex-wrap justify-center md:gap-[61px] gap-[40px]'>
-                        {STATS.map((stat, index) => (
-                            <div key={index}>
-                                <p className='md:text-customLg text-5xl font-normal text-light-orange max-sm:text-center'>{stat.value}</p>
-                                <p className='font-bold md:text-[21px] text-lg whitespace-nowrap'>{stat.label}</p>
+                    <div
+                        ref={ref}
+                        className="flex max-md:gap-6 max-md:flex-wrap max-md:justify-center max-md:items-center"
+                    >
+                        {STATS.map((item, index) => (
+                            <div
+                                key={index}
+                                className={`flex flex-col justify-center items-center ${index === 0 ? "md:mr-[71px]" : index === 1 && "md:mr-[54px]"
+                                    }`}
+                            >
+                                <h3 className="text-customLg max-lg:text-7xl max-md:text-6xl max-sm:text-5xl max-lg:leading-[92px] max-md:leading-[80px] max-sm:leading-[60px] leading-[100px] text-light-orange tracking-[-0.66px]">
+                                    +
+                                    <CountUp
+                                        start={0}
+                                        end={
+                                            count && index === 0
+                                                ? 155
+                                                : count && index === 1
+                                                    ? 325
+                                                    : count && index === 2
+                                                        ? 250
+                                                        : 0
+                                        }
+                                        duration={3}
+                                    />
+                                </h3>
+                                <p className="font-bold text-[21px] leading-[100%] max-lg:text-xl max-md:text-lg max-sm:text-base text-center">
+                                    {item.label}
+                                </p>
                             </div>
                         ))}
                     </div>
